174e503c73d7df5a2f71e14e13caabd0298f54e1
Dennis Muoki <muokid3@gmail.com>
2022-04-05 20:19:10 +0300

    passing data from controller to view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $featuredProducts = Product::where('is_featured', true)->get();
+        return view('home')->with([
+           'featuredProducts' => $featuredProducts
+        ]);
     }
 }
--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,11 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
     public function category_products($category_id){
-        //todo
+
+        //$category = Category::find($category_id);
+        $products = Product::where('category_id',$category_id)->get();
+        $categories = Category::all();
+
+//        $productsCount = Product::where('category_id',$category_id)->count();
+
+        return view('category_products')->with([
+            'products' => $products,
+            'productsCount' => $products->count(),
+            'categories' => $categories
+        ]);
     }
 }
